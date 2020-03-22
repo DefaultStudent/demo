faabd6f61ee8aaa95a49a38cc0c574af2c0a7127
96495 <964956769@qq.com>
2020-03-22 19:27:42 +0800

    First

---
+++ src/main/java/com/example/web/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+
+}
