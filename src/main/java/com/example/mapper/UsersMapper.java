040941c9fd027e3d16ea6e4e381b3ac0a0df3a5b
96495 <964956769@qq.com>
2020-04-25 10:23:36 +0800

    修改UserMapper.xml、application.yml文件 - 20200425

--- src/main/java/com/example/mapper/UsersMapper.java
+++ src/main/java/com/example/mapper/UsersMapper.java
@@ -1,14 +1,26 @@
 package com.example.mapper;
 
 import com.example.entity.Users;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Mapper
 @Repository
 public interface UsersMapper {
 
+    /**
+     * 获取全部用户信息
+     * @return
+     */
     List<Users> getAll();
-    Users loginPage(String id, String name);
+
+    /**
+     * 根据用户Id和密码获取用户信息
+     * @param id 用户Id
+     * @param pwd 用户密码
+     * @return
+     */
+    Users loginPage(String id, String pwd);
 }
